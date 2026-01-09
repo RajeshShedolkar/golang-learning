4f652e61c5c03444ec92ac2910364ce8f0d84e18
Rajesh Shedolkar <rajesh.shedolkar@in.ey.com>
2026-01-09 06:55:27 +0530

    sum.go

--- 2026/sum.go
+++ 2026/sum.go
@@ -1,3 +1,13 @@
-func sum(arr []int){
-	
+package main
+
+import "fmt"
+
+func Sum(arr []int) int {
+	s := 0
+	for i, v := range arr {
+		fmt.Println("loop number -->", i)
+		s += v
+	}
+	fmt.Println(s)
+	return s
 }
