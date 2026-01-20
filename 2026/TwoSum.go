f26ce90662bed94a3de8d4aa4882adeb76f5f388
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-01-20 23:48:09 +0530

    TwoSum Pbm

---
+++ 2026/TwoSum.go
@@ -0,0 +1,16 @@
+package main
+
+func TwoSumPbm(arr []int, target int) []int {
+
+	hash := make(map[int]int)
+
+	for index, curr_ele := range arr {
+		diff := target - curr_ele
+		if i, ok := hash[diff]; ok {
+			return []int{index, i}
+		}
+		hash[curr_ele] = index
+	}
+	return nil
+
+}
