db9e844f7fd4c035cc6cdfda412d66cbc2a4ca49
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-01-21 07:44:41 +0530

    CheckSortedArray

--- 2026/gen.go
+++ 2026/gen.go
@@ -22,11 +22,11 @@ func MaxEle(arr []int) int {
 	return max
 }
 
-func ReverseArray(arr *[]int){
+func ReverseArray(arr *[]int) {
 	start, end := 0, len(*arr)-1
-	mid := len(*arr)/2
+	mid := len(*arr) / 2
 	fmt.Println(start, end, mid)
-	for i:=0;i<mid;i++{
+	for i := 0; i < mid; i++ {
 		(*arr)[start], (*arr)[end] = swap((*arr)[start], (*arr)[end])
 		start += 1
 		end -= 1
@@ -34,7 +34,7 @@ func ReverseArray(arr *[]int){
 	fmt.Println(*arr)
 }
 
-func swap(a int, b int)(int, int){
+func swap(a int, b int) (int, int) {
 	temp := a
 	a = b
 	b = temp
@@ -43,10 +43,20 @@ func swap(a int, b int)(int, int){
 
 func ReverseArray2(arr []int) {
 	start, end := 0, len(arr)-1
-
 	for start < end {
 		arr[start], arr[end] = arr[end], arr[start]
-		start++
-		end--
+		start += 1
+		end -= 1
 	}
+}
+
+func CheckArraySorted(arr []int)bool{
+	first := 0
+	for first < len(arr)-1{
+		if arr[first] > arr[first+1]{
+			return false
+		}
+		first+=1
+	}
+	return true
 }
