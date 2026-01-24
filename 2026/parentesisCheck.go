da285109ef6748ad46208d5fcdc449079515420b
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-01-25 01:01:51 +0530

    PrenthesisMatch

--- 2026/parentesisCheck.go
+++ 2026/parentesisCheck.go
@@ -1,5 +1,6 @@
 package main
-import "fmt"
+
+//import "fmt"
 
 func ValidateParenthesis(s string) bool {
 	// {(abc+xyz*[1+2])}()
@@ -12,16 +13,32 @@ func ValidateParenthesis(s string) bool {
 		if sCurrChar == "(" || sCurrChar == "{" || sCurrChar == "[" {
 			top += 1
 			stack[top] = currChar
-			
+
 		}
 		if sCurrChar == ")" || sCurrChar == "}" || sCurrChar == "]" {
-			if len(stack) > 0 && !isParenthesisMatchCaseStyle(string(stack[top]), sCurrChar) {
+			if len(stack) > 0 && !isParenthesisMatchHashStyle(string(stack[top]), sCurrChar) {
 				return false
 			}
 			top -= 1
 		}
 	}
 	return true
+}
+
+func isParenthesisMatchHashStyle(p1 string, p2 string) bool {
+	hash := map[rune]rune{
+		'{': '}',
+		'(': ')',
+		'[': ']',
+	}
+	r1 := rune(p1[0])
+	r2 := rune(p2[0])
+	if v, ok := hash[r1]; ok {
+		if v == r2 {
+			return true
+		}
+	}
+	return false
 }
 
 func isParenthesisMatchCaseStyle(p1 string, p2 string) bool {
