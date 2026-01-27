405ed9dc8a35aefa11099bd727e64140ac294e2f
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-01-27 23:26:52 +0530

    Queue using arr

--- 2026/queue.go
+++ 2026/queue.go
@@ -1,40 +1,48 @@
 package main
+
 import "fmt"
 
-type Queue struct{
-	arr []int
-	front int
-	rear int
-	size int
+type Queue struct {
+	arr      []int
+	front    int
+	rear     int
+	size     int
 	capacity int
 }
 
-func InitQueue(capacity int) *Queue{
+func InitQueue(capacity int) *Queue {
 	return &Queue{
-		arr: make([]int, capacity),
-		front: 0,
-		rear: -1,
-		size: 0,
+		arr:      make([]int, capacity),
+		front:    0,
+		rear:     -1,
+		size:     0,
 		capacity: capacity,
 	}
 }
 
-func (q *Queue) EnQueue(data int){
-	q.arr = append(q.arr, data)
+func (q *Queue) EnQueue(data int) {
+	if q.size == q.capacity {
+		fmt.Println("Queue is full")
+		return
+	}
+
+	q.rear = (q.rear + 1) % q.capacity
+	q.arr[q.rear] = data
+	q.size += 1
 	fmt.Println("After EnQueue: ", q.arr)
 }
 
-func (q *Queue) DeQueue()(int, bool){
-	if len(q.arr)==0{
+func (q *Queue) DeQueue() (int, bool) {
+	if q.size == 0 {
 		fmt.Println("No more element left to DqQueue")
 		return 0, false
 	}
-	deQuVal := q.arr[0]
-	q.arr = q.arr[1:]
+	deQuVal := q.arr[q.front]
+	q.front = (q.front + 1) % q.capacity
+	q.size -= 1
 	fmt.Println("After DeQueue: ", q.arr)
-	return deQuVal, true 
+	return deQuVal, true
 }
-
 
 // func main(){
 
