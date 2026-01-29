536532d7560572503d7b547174a9a1bafe955001
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-01-29 22:37:52 +0530

    levelOrderTraversal

--- 2026/bfs_tree.go
+++ 2026/bfs_tree.go
@@ -2,23 +2,38 @@ package main
 
 import "fmt"
 
-type Node struct{
-	val int
-	left *Node
+type Node struct {
+	val   int
+	left  *Node
 	right *Node
 }
 
+// type Queue struct{
+// 	arr []int
+// 	capacity int
+// 	size int
+// 	front int
+// 	rear int
+// }
 
-func NewNode(val int)*Node{
+// func (q *Queue) EnQueue(){
+
+// }
+
+// func (q *Queue) DeQueue(){
+
+// }
+
+func NewNode(val int) *Node {
 	return &Node{
-		val: val,
-		left: nil,
+		val:   val,
+		left:  nil,
 		right: nil,
 	}
 }
 
-func inorder(root *Node){
-	if root==nil{
+func inorder(root *Node) {
+	if root == nil {
 		return
 	}
 	inorder(root.left)
@@ -26,9 +41,35 @@ func inorder(root *Node){
 	inorder(root.right)
 }
 
-func main(){
+func LevelOrder(root *Node) {
+	if root == nil {
+		return
+	}
+
+	queue := []*Node{root}
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+
+		fmt.Print(curr.val, " ")
+
+		if curr.left != nil {
+			queue = append(queue, curr.left)
+		}
+		if curr.right != nil {
+			queue = append(queue, curr.right)
+		}
+	}
+}
+
+func main() {
 	root := NewNode(1)
 	root.left = NewNode(2)
 	root.right = NewNode(3)
-	inorder(root)
+	root.left.left = NewNode(4)
+	root.left.right = NewNode(12)
+	root.left.left.left = NewNode(5)
+	//inorder(root)
+	LevelOrder(root)
 }
