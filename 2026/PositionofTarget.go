290eca43447f0f0dec5975a6d0bc5fc113c1f024
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-02-08 23:16:25 +0530

    SearchRange by BinarySearch

--- 2026/PositionofTarget.go
+++ 2026/PositionofTarget.go
@@ -2,97 +2,54 @@ package main
 
 import "fmt"
 
-func positionOfTarget(arr []int, target int) []int {
-	ind, err := getTarget(arr, target)
-	start, end := ind, ind
-	if err != nil {
-		return []int{-1, -1}
+func searchRange(nums []int, target int) []int {
+	return []int{
+		findFirst(nums, target),
+		findLast(nums, target),
 	}
-
-	for start-1 >= 0 {
-		if arr[start-1] == arr[ind] {
-			start -= 1
-		} else {
-			break
-		}
-	}
-
-	for end+1 < len(arr) {
-		if arr[end+1] == arr[ind] {
-			end += 1
-		} else {
-			break
-		}
-	}
-
-	return []int{start, end}
 }
 
-func positionOfTarget2(arr []int, target int) []int {
-	ind, err := getTarget(arr, target)
-	if err != nil {
-		return []int{-1, -1}
-	}
+func findFirst(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	res := -1
 
-	min_ind := getMinInd(arr, ind)
-	max_ind := getMaxInd(arr, ind)
-
-	return []int{min_ind, max_ind}
-
-}
-
-func getMinInd(arr []int, ind int) int {
-	start, end := 0, ind-1
-	target := arr[ind]
-	min_ind := ind
 	for start <= end {
 		mid := (start + end) / 2
-		if arr[mid] == target {
-			min_ind = mid
+		if nums[mid] == target {
+			res = mid
 			end = mid - 1
-		} else {
-			start = mid + 1
-		}
-	}
-	return min_ind
-}
-
-func getMaxInd(arr []int, ind int) int {
-	start, end := ind+1, len(arr)-1
-	target := arr[ind]
-	max_ind := ind
-	for start <= end {
-		mid := (start + end) / 2
-		if arr[mid] == target {
-			max_ind = mid
+		} else if nums[mid] < target {
 			start = mid + 1
 		} else {
 			end = mid - 1
 		}
 	}
-	return max_ind
+	return res
 }
 
-func getTarget(arr []int, target int) (int, error) {
-	start, end := 0, len(arr)-1
+func findLast(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	res := -1
+
 	for start <= end {
 		mid := (start + end) / 2
-		if arr[mid] == target {
-			return mid, nil
-		} else if arr[mid] > target {
-			end = mid - 1
-		} else {
+		if nums[mid] == target {
+			res = mid
 			start = mid + 1
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
 		}
 	}
-	return -1, fmt.Errorf("No Target found")
-
+	return res
 }
+
 
 func main() {
 	arr := []int{1, 2, 2, 2, 2, 2, 3, 4, 5}
 	target := 2
 	fmt.Println("Input arr: ", arr, "Target: ", target)
-	fmt.Println(positionOfTarget2(arr, target))
+	fmt.Println(searchRange(arr, target))
 
 }
