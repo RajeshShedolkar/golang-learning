8a08f172ad5f71720785cbf5cd3282297d8f0536
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-02-11 23:25:56 +0530

    max of sum of k size of sub arr

--- 2026/findMaxSumFromSubArray.go
+++ 2026/findMaxSumFromSubArray.go
@@ -1,34 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func MaxSumOfSubArr(arr []int, size int) (int, error) {
 	if len(arr) == 0 {
-		return 0, fmt.Errorf("Empty Arr")
+		return 0, errors.New("array is empty")
+	}
+
+	if size <= 0 || size > len(arr) {
+		return 0, errors.New("invalid window size")
 	}
 
 	currSum := 0
 	for _, v := range arr[:size] {
 		currSum += v
 	}
-	k := size
+
 	maxSum := currSum
-	for k < len(arr) {
+
+	for k := size; k < len(arr); k++ {
 		currSum -= arr[k-size]
 		currSum += arr[k]
-		if maxSum < currSum {
+
+		if currSum > maxSum {
 			maxSum = currSum
 		}
-		k += 1
 	}
+
 	return maxSum, nil
 }
 
 func main() {
 	arr := []int{2, 1, 5, 1, 3, 2}
 	size := 3
-	fmt.Println(MaxSumOfSubArr(arr, size))
 
+	ans, err := MaxSumOfSubArr(arr, size)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Max Sum:", ans)
 }
