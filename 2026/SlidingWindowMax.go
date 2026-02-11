a04a23f5c3a41dd92c066d34bfe61119ea6aae63
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-02-11 23:11:45 +0530

    arr of max in Slding Window

--- 2026/SlidingWindowMax.go
+++ 2026/SlidingWindowMax.go
@@ -6,30 +6,9 @@ func maxSlidingWindow(nums []int, k int) []int {
 	if len(nums) == 0 || k == 0 {
 		return []int{}
 	}
+	// Write the Logic
 
-	deque := make([]int, 0) // stores indices
 	result := make([]int, 0)
-
-	for i := 0; i < len(nums); i++ {
-
-		// 1. Remove indices out of window
-		if len(deque) > 0 && deque[0] <= i-k {
-			deque = deque[1:]
-		}
-
-		// 2. Remove smaller elements from back
-		for len(deque) > 0 && nums[deque[len(deque)-1]] < nums[i] {
-			deque = deque[:len(deque)-1]
-		}
-
-		// 3. Add current index
-		deque = append(deque, i)
-
-		// 4. Add max to result once window is formed
-		if i >= k-1 {
-			result = append(result, nums[deque[0]])
-		}
-	}
 
 	return result
 }
@@ -40,4 +19,3 @@ func main() {
 
 	fmt.Println(maxSlidingWindow(nums, k))
 }
-
