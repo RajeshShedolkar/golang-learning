ca3829c707f61d26346335632f5621c7e5441da3
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2026-02-22 22:08:34 +0530

    Painter's pbm

--- 2026/PainterProblem.go
+++ 2026/PainterProblem.go
@@ -1,2 +1,48 @@
 package main
 
+func getMax(arr []int) int {
+	if len(arr)==0{
+		return 0
+	}
+	maxVal := arr[0]
+	for _, v := range arr{
+		if maxVal < v{
+			maxVal = v
+		}
+	}
+	return maxVal
+}
+
+
+func getSum(arr []int)int{
+	sum := 0
+	for _, v := range arr{
+		sum += v
+	}
+	return sum
+}
+
+
+func isFeasible(boards []int, k int, T int)bool{
+
+	return true
+}
+
+func MinTime(boards []int, k int)int{
+	minTime := getMax(boards)
+	maxTime := getSum(boards)
+	T := 0
+	for minTime < maxTime {
+		T = (minTime + maxTime) /2 
+		if isFeasible(boards, k, T){
+			maxTime = T - 1
+		}else{
+			minTime = T + 1
+		}
+	}
+	return T
+}
+
+func main(){
+
+}
